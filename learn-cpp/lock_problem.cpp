3533af54036fe15a9402678bc0650df633e303cb
shuming.lsm <shuming.lsm@alibaba-inc.com>
2022-05-26 16:39:02 +0800

    init learn-cpp/learn-boost

---
+++ learn-cpp/lock_problem.cpp
@@ -0,0 +1,126 @@
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+// see: http://howardhinnant.github.io/dining_philosophers.html#Polite
+using namespace std;
+using namespace std::chrono_literals;
+
+std::mutex m1;
+std::mutex m2;
+
+
+void
+f1()
+{
+    while (true)
+    {
+        std::unique_lock<std::mutex> l1(m1, std::defer_lock);
+        std::unique_lock<std::mutex> l2(m2, std::defer_lock);
+        std::lock(l1, l2);
+        std::cout << "f1 has the two locks\n";
+        std::this_thread::sleep_for(100ms);
+    }
+}
+
+void
+f2()
+{
+    while (true)
+    {
+        std::unique_lock<std::mutex> l2(m2, std::defer_lock);
+        std::unique_lock<std::mutex> l1(m1, std::defer_lock);
+        std::lock(l2, l1);
+        std::cout << "f2 has the two locks\n";
+        std::this_thread::sleep_for(100ms);
+    }
+}
+
+template<class L0>
+void lock(L0& l0, L0& l1) {
+    if (l0.mutex < l1.muxt) {
+        unique_lock<L0> u0(l0);
+        l1.lock();
+        u0.realase();
+    } else {
+        unique_lock<L0> u1(l1);
+        l0.lock();
+        u1.release();
+    }
+}
+
+void low_load() {
+    std::thread t1(f1);
+    std::thread t2(f2);
+    t1.join();
+    t2.join();
+}
+
+// Problem: load high
+std::mutex mutex1, mutex2;
+std::condition_variable cond_var;
+bool cond = false;
+std::atomic<bool>done{false};
+void Take_Locks()
+{
+    while( !done )
+    {
+        std::this_thread::sleep_for( 1s );
+
+        std::unique_lock<std::mutex> lock1( mutex1, std::defer_lock );
+        std::unique_lock<std::mutex> lock2( mutex2, std::defer_lock );
+        std::lock( lock1, lock2 );
+        std::cout << "Take_Locks id:" << this_thread::get_id() << " waiting \n";
+
+        std::this_thread::sleep_for( 1s );
+        lock1.unlock();
+        lock2.unlock();
+        std::cout << "Take_Locks id:" << this_thread::get_id() << " done. \n";
+    }
+}
+
+void Conditional_Code()
+{
+    std::unique_lock<std::mutex> lock1( mutex1, std::defer_lock );
+    std::unique_lock<std::mutex> lock2( mutex2, std::defer_lock );
+
+    std::lock( lock1, lock2 );
+    std::cout << "Conditional id:" << this_thread::get_id() << " waiting \n";
+
+    while( !cond )
+        cond_var.wait( lock1 );
+
+    std::cout << "t4: condition met \n";
+}
+
+void bad_case()
+{
+    std::thread t1( Take_Locks ), t2( Take_Locks ), t3( Take_Locks );
+    std::thread t4( Conditional_Code );
+
+    std::cout << "threads started \n";
+    std::this_thread::sleep_for( 10s );
+
+    std::unique_lock<std::mutex> lock1( mutex1 );
+    std::cout << "mutex1 locked \n" ;
+    std::this_thread::sleep_for( 5s );
+
+    std::cout << "setting condition/notify \n";
+    cond = true;
+    cond_var.notify_one();
+    std::this_thread::sleep_for( 5s );
+
+    lock1.unlock();
+    std::cout << "mutex1 unlocked \n";
+    std::this_thread::sleep_for( 6s );
+
+    done = true;
+    t4.join(); t3.join(); t2.join(); t1.join();
+}
+
+int main() {
+    bad_case();
+}
