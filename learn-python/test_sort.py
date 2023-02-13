d54dd6d27c56f24977380c82a9a997ad3647220b
Kevin Li <ming.moriarty@gmail.com>
2023-02-13 16:17:27 +0800

    add python script

--- learn-python/test_sort.py
+++ learn-python/test_sort.py
@@ -14,5 +14,39 @@ def test_sort_1():
             print("-----------j:%s i:%s--------" % (j, i))
             print(datas[j:i])
 
+def print1():
+    t1 = """
+    @Ignore
+    @Test
+    public void testQuery<SEQ>() {
+        runFileUnitTest("materialized-view/tpch/q<SEQ>");
+    }
+    """
+    for i in range (1, 23):
+        tt = t1.replace("<SEQ>", str(i))
+        print(tt)
 
-test_sort_1()
+def handle_files(f_name):
+    fp = open(f_name)
+    content = ""
+    flag = True
+    for line in fp.readlines():
+        if "[result]" in line:
+            flag = False
+            continue
+        if "[end]" in line:
+            flag = True
+        if not flag:
+            continue
+        content += line
+    print(content)
+    fp.close()
+
+if __name__ == "__main__":
+    # test_sort_1()
+    # print1()
+    f_dir = "/Users/lishuming/work/starrocks/fe/fe-core/src/test/resources/sql/materialized-view/tpch"
+    for i in range(1, 23):
+        f_name = f_dir + "/q" + str(i) + ".sql"
+        handle_files(f_name)
+        breawk
